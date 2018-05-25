850ed61f236b1cff4cb400da0195e9527599a069
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2018-05-25 13:38:29 -0500

    Create a18.py

---
+++ a18.py
@@ -0,0 +1,10 @@
+# Example code showing how to define a dictionary and add a new key/value pair to it
+person = {'name': 'Bob', 'dob': '12/03/1975', 'gender':'male'}
+person['height'] = 74
+print(person)
+
+# Example code showing how to iterate over the keys and values in a dictionary
+for key in person:
+    print(key, ":", person[key])
+
+# >>>>>>>>>>  END OF EXAMPLES <<<<<<<<<<<<
