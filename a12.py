4a8b6295200714eb04fc54216e49cf8ac59cd3ca
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2018-05-25 13:32:47 -0500

    Create a12.py

---
+++ a12.py
@@ -0,0 +1,9 @@
+# Create a blank array
+array = []
+number=input("How many elements do you want?: ")
+# Now use a for loop to add to the array.
+
+for x in range (int(number)):
+  n = input("Enter Number: ")
+  array.append(int(n))
+print (array)
