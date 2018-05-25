c670c6264e569c4825ef90304fb3d57a2af7dd32
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2018-05-25 13:22:52 -0500

    Create a5.py

---
+++ a5.py
@@ -0,0 +1,9 @@
+# This is an array that contains different foods
+food_items = ["turkey","ham", "spam","eggs","nuts"]
+print (food_items[3])
+
+# Use a for loop to print all of the food items
+print("This will print the array of food items")
+# Hint:  for x in food_items:
+for x in food_items:
+  print (x)
