fed6c0adca2a8cd1d3d1e5a0b548608b3dd9ef3a
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2018-05-25 13:44:42 -0500

    Create a32.py

---
+++ a32.py
@@ -0,0 +1,10 @@
+#replacing strings
+
+# Read a string:
+old_str = str("bilbo.baggins@bagend.hobbiton.shire.me")
+# s = input()
+s = input("What Character Do You Want To Remove: ")
+# Print a string:
+new_str = old_str.replace(s, "")
+# print(s)
+print(new_str)
