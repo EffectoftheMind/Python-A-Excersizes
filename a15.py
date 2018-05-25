fabaa29d69a443be000851d118f825790b0498c9
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2018-05-25 13:35:06 -0500

    Create a15.py

---
+++ a15.py
@@ -0,0 +1,19 @@
+# Read a string:
+# s = input()
+# Print a string:
+# print(s)
+
+s = input()
+
+print(s[2])
+print(s[-2])
+print(s[0:5])
+print(s[0:9])
+print(s[::2])
+print(s[1::2])
+print(s[::-1])
+print(s[::-2])
+print(len(s))
+
+
+#slicing strings
