3d5200c21651a691ff84f78ae8d5af38db4c8c82
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2020-09-15 20:10:35 -0500

    Update a15.py

--- a15.py
+++ a15.py
@@ -1,6 +1,1 @@
-# create a function to check for duplicates in an array
-   #Use this array in your program.
 
-test_array = [5,51,3,3,1,2,89,54,3,5,41,4,65,12,77,321,4]
-test_array = list(set(test_array))
-print (test_array)
